923326ae90232b9e3bc04accb5f226a8f32e9bbd
OULION <thomas.oulion@gmail.com>
2015-12-15 10:51:42 +0100

    premier exemple de commit avec le contenu global

---
+++ src/main/java/mediatic/Test.java
@@ -0,0 +1,14 @@
+package mediatic;
+
+import com.google.common.base.Strings;
+
+import worker.MyStringUtils;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		System.out.println(MyStringUtils.isNullOrEmpty("hello world"));
+		System.out.println(Strings.repeat("hello world", 10));
+	}
+}
